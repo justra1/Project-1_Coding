f87012475acb948d7de4898127894e01b4db33c1
Film 06 <nitiwatkantawa@gmail.com>
2024-12-04 16:07:07 +0700

    Add files via upload

---
+++ 1form_06Film.py
@@ -0,0 +1,15 @@
+print('Welcome to Pre-Engineering & Robotics')
+
+num = input('Number of Student : ')
+name = input('Name : ')
+surname = input('Surname : ')
+grade = input('Grade : ')
+
+
+
+
+print(f'Number is : {num}')
+print(f'History : {name} {surname} {grade}')
+print('Your history form is confirm')
+print()
+print('Good luck have fun 😁')
